01b60e105dfb581f5301475ef1ad2940ae171b81
David C <dvd.cm6@gmail.com>
2020-04-17 17:11:54 +0200

    Add files via upload

---
+++ joc.hs
@@ -0,0 +1,17 @@
+--	David Carballo
+--	Programa que permet jugar al quatre en ratlla contra l'ordinador
+
+import System.Random
+
+randInt :: Int -> Int -> IO Int
+-- randInt low high is an IO action that returns a
+-- pseudo-random integer between low and high (both included).
+
+
+
+--	Estratègia RANDOM
+
+--	Estratègia GREEDY
+
+--	Estratègia SMART
+
